26e7d5997b2a9a3242963d68ca80453920a77b4d
NaveKabirKhan <navedkhan8495@gmail.com>
2024-08-31 13:33:17 +0530

    add mongo confirmation

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
